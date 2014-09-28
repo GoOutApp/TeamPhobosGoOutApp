e34994e716ecede9d37180d026411d415c630eb9
Ivan Rusekov <rusekov@gmail.com>
2014-09-28 12:45:12 -0500

    Changes made by Ivan Rusekov (rusekov@gmail.com). Adding Cammera api for user photos. adding #image max width in main.css

---
+++ GoOutApp/scripts/app/homepage.js
@@ -0,0 +1,72 @@
+/* jQuerry, app*/
+
+var app = app || {};
+
+app.homepage = (function () {
+    'use strict';
+
+   var everlive = new Everlive("39dbaEzAeRl1ddLP");
+
+    //homepage model
+    var homepageViewModel = (function () {
+
+        $("#imageAdd").hide();
+        $("#imageLoad").show();
+
+        var addImage = function () {
+
+            var success = function (data) {
+                everlive.Files.create({
+                    Filename: app.Users.currentUser.get('data').Username + ".jpg",
+                    ContentType: "image/jpeg",
+                    base64: data
+                }).then(loadImage)
+            };
+            var error = function () {
+                navigator.notification.alert("Unfortunately we could not add the image");
+            };
+            var config = {
+                destinationType: Camera.DestinationType.DATA_URL,
+                allowEdit: true,
+                quality: 50,
+                targetHeight: 400,
+                targetWidth: 400
+            };
+            navigator.camera.getPicture(success, error, config);
+        };
+
+        var loadImage = function () {
+            $("#imageAdd").show();
+            $("#imageLoad").hide();
+
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                files.reverse()
+
+                data.result.forEach(function (image) {
+
+                    console.log(image.Uri);
+                    console.log(image.Filename);
+                    if (image.Filename === app.Users.currentUser.get('data').Username + ".jpg")
+                    {
+                        files.push(image.Uri);
+                    }
+                });
+
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        };
+    
+        return {
+            addImage: addImage,
+            loadImage: loadImage
+        };        
+    }());
+    
+    return homepageViewModel;
+}());
+
+   
